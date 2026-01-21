8a6d09c42163da845ac1c4ffedbed667c89fea22
Marah N <marah.n35@gmail.com>
2026-01-21 14:19:54 +0200

    created course module and controller with their db and data in json file

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'ref_code', 'duration', 'description',
+        'about_course', 'image_path', 'category'
+    ];
 }
